c27fda8a734633ce602e4c49b8aab0f8761a953f
ArunEswarramurthy <aruneswarramurthy@gmail.com>
2025-04-20 02:24:04 +0530

    change

---
+++ finding positive and negative number .java
@@ -0,0 +1,12 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if (num <= 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is positive");
+        }
+    }
+}
